d4bf6ac99964a314ff2b829a8966d69c2f1cff25
GALHEO <theogalvani4@gmail.com>
2026-05-13 11:03:41 +0200

    try

--- formation.js
+++ formation.js
@@ -1,10 +1,40 @@
+function sleep(millis)
+{
+    var date = new Date();
+    var curDate = null;
+    do { curDate = new Date(); }
+    while(curDate-date < millis);
+}
 
 const div = document.getElementById("rameau");
 const text = document.getElementById("text");
 const img = document.getElementById("imageR");
+
+
 const hT = text.clientHeight;
-const hi=div.getBoundingClientRect().height;
-div.style.height = hT + "px";
+const wI = img.clientWidth-div.clientWidth;
+let hD;
+
+
+
+const h1 = img.clientHeight;
+
+// Hauteur avec coordonnées précises
+const h2 = img.getBoundingClientRect().height;
+
+// Hauteur intrinsèque de l'image
+const h3 = img.height;
+
+// Hauteur avec padding et bordures
+const h4 = img.offsetHeight;
+console.log(h1, h2, h3, h4);
+
+
 console.log(hT, div.clientHeight, div.clientWidth, text.clientHeight, img.clientHeight, img.clientWidth, img.getBoundingClientRect().height);
+console.log(hT, hD, wI);
+console.log(text.offsetTop, div.offsetTop, text.offsetTop-div.offsetTop);
+div.style.height = hT + "px";
+//text.style.transform = "translateY(" + (-hi) + "px)";
 //text.style.transform = "translateY(" + (-hT) + "px)";
-text.style.top = hi + "px";
+text.style.top = -hD + "px";
+img.style.transform = "translateX(" + (-wI) + "px)";
