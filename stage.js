049d7e40bd235f460cd405925140d28a2a0c60ec
GALHEO <theogalvani4@gmail.com>
2026-05-27 14:04:46 +0200

    Mise à jour des stages

--- stage.js
+++ stage.js
@@ -1,19 +1,16 @@
-let affiche = false;
 function show(){
     let button = document.getElementById("button");
     let list = document.getElementById("list");
-    console.log(list.style.height, list.height, list.clientHeight, list.children[0].clientHeight);
+    //console.log(list.style.height, list.height, list.clientHeight, list.children[0].clientHeight);
     let childH=0;
     for(let i=0; i<list.children.length; i++){
         childH+=list.children[i].clientHeight;
     }
-    console.log(childH);
-    if (affiche){
-        affiche = false;
+    //console.log(childH);
+    if (list.style.height === childH + 'px'){
         button.textContent = "dates ᐱ";
         list.style.height = "0px";
     }else{
-        affiche = true;
         button.textContent = "dates ᐯ";
         list.style.height = childH + 'px';
     }
